17b77eb3d030617fb120de82989a70abe626e717
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2020-12-19 19:48:25 -0300

    Versão final

--- scriptex2.js
+++ scriptex2.js
@@ -4,11 +4,9 @@ function verificacao() {
   var anoNascimento = document.getElementById('anoNascimento')
   var msgAlerta = document.getElementById('alerta')
   var sexo = document.getElementsByName('sexo')
-  //var sexoFem = document.getElementById('feminino')
-  var homem = document.getElementById('homem')
-  var mulher = document.getElementById('mulher')
-  //imagem.setAttribute('id', 'foto')
-  var genero = ""
+  var imagem = document.getElementById('imagem')
+ 
+
 
   if (anoNascimento.value.length == 0 || anoNascimento.value > anoAtual) {
     msgAlerta.innerHTML = `Verifique`
@@ -16,16 +14,15 @@ function verificacao() {
   } else {
     var idade = anoAtual - Number(anoNascimento.value)
   }
-
+  
   if (sexo[0].checked) {
     genero = "masculino"
     document.body.style.background = "blue"
-    homem.style.display = "block"
+    imagem.src="./image/foto-homem.jpg"
   } else {
     genero = "feminino"
     document.body.style.background = "red"
-    mulher.style.display = "block"
+    imagem.src="./image/foto-mulher.jpg"
   }
   msgAlerta.innerHTML = `Você é do sexo ${genero} e tem ${idade} anos`
-  //msgAlerta.appendChild(imagem)
 }
